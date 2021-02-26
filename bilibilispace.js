03c853fc82499ed277b856ccc7ed9bfe5c0cf277
zxcvbnmip <41607914+zxcvbnmip@users.noreply.github.com>
2021-02-26 15:13:47 +0800

    Update bilibilispace.js

--- bilibilispace.js
+++ bilibilispace.js
@@ -1,3 +1,9 @@
+/*
+bilibili remove some account modules. by onewayticket255
+QX:
+^https://app.bilibili.com/x/v2/space\?access_key script-response-body https://raw.githubusercontent.com/zxcvbnmip/quantumultxjs/master/bilibilispace.js
+*/
+
 //收藏排行前10，长按通知进入（iOS13以下通知中心不支持）
 let url = $request.url
 let regex = /vmid=(\d*)/
